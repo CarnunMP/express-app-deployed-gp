e0c15677af3762efaa375151bb61ec405f9925b0
CarnunMP <carnun@hotmail.co.uk>
2019-10-31 09:24:24 +0000

    spun up with dummy data

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+const port = process.env.PORT || 4000;
+const friends = [
+  { id: 1, name: 'Shaun'},
+  { id: 2, name: 'Megan'},
+  { id: 3, name: 'Pere'},
+];
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/api/friends', (req, res) => {
+  res.json(friends);
+})
+
+server.listen(port, () => {
+  console.log(`\n *Server listening on port ${port}* \n`);
+})
